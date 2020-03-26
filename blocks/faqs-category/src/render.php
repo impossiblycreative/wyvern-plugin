f05c5b60cb55c414a49f0bb3a7dfbb767e130b87
Adam Soucie <adamsoucie@gmail.com>
2020-03-26 18:05:23 -0400

    Accordion portion of FAQs Category block complete

--- blocks/faqs-category/src/render.php
+++ blocks/faqs-category/src/render.php
@@ -6,6 +6,7 @@ function faqs_category_render_callback( $attributes, $content = '' ) {
     // Get our data
     $block_classes          = ( $attributes['className'] ) ? $attributes['className'] : 'wp-block-wyvern-plugin-faqs-category';
     $block_id               = rand( 0, 100 );
+    $faq_counter            = 0;
 
     // Begin our output
     ob_start();
@@ -26,14 +27,56 @@ function faqs_category_render_callback( $attributes, $content = '' ) {
         <?php if ( $faqs->have_posts() ) : ?>
             <div class="alignfull <?php echo esc_html( $block_classes ); ?>">
                 <h2 class="faqs-block-header"><?php echo esc_html_e( 'Frequently Asked Questions', 'wyvern-plugin' ); ?></h2>
-                <?php while( $faqs->have_posts() ) : ?>
-                    <?php $faqs->the_post(); ?>
 
-                    <div class="faq">
-                        <h3 class="faq-header"><?php echo esc_html( get_the_title() ); ?></h3>
-                        <div class="faq-content"><?php echo wp_kses_post( get_the_content() ); ?></div>
-                    </div>
-                <?php endwhile; ?>
+                <div id="faqs-<?php echo $block_id ?>" class="faqs-container accordion-container">
+                    <?php while( $faqs->have_posts() ) : ?>
+                        <?php $faqs->the_post(); ?>
+                        <?php $faq_counter++; ?>
+
+                        <div id="faq-<?php echo $faq_counter; ?>" class="faq">
+                            <h3 id="faq-<?php echo $faq_counter; ?>-header" class="faq-header">
+                                <button class="accordion-trigger" aria-controls="faq-<?php echo $faq_counter; ?>-content" aria-expanded="<?php echo ( $faq_counter === 1 ) ? 'true' : 'false' ; ?>" <?php echo ( $faq_counter === 1 ) ? 'aria-disabled="true"' : ''; ?>>
+                                    <span class="faq-header-text"><?php echo esc_html( get_the_title() ); ?></span>
+                                    <span class="fas <?php echo ( $faq_counter === 1 ) ? 'fa-minus' : 'fa-plus'; ?>"></span>
+                                </button>
+                            </h3>
+                            <div id="faq-<?php echo $faq_counter; ?>-content" class="faq-content accordion-panel" role="region" aria-labelledby="faq-<?php echo $faq_counter; ?>-header" <?php echo ( $faq_counter === 1 ) ? '' : 'hidden'; ?>>
+                                <?php echo wp_kses_post( get_the_content() ); ?>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
+
+                <!-- Accordion Controlling Script -->
+                <script>
+                    var accordionControls = ( function() {
+                        const triggers = document.querySelectorAll( '#faqs-<?php echo $block_id; ?> .accordion-trigger' );
+
+                        function toggleAccordion( event ) {
+                            let trigger = event.target;
+
+                            // Make sure we have the actual trigger
+                            if ( trigger.tagName === 'SPAN' ) {
+                                trigger = trigger.parentNode;
+                            }
+
+                            // Set the ARIA Expanded
+                            const expanded = ( trigger.getAttribute( 'aria-expanded' ) === 'true' ) ? false : true;
+                            trigger.setAttribute( 'aria-expanded', expanded );
+
+                            // Toggle the content display
+                            const content = document.getElementById( trigger.getAttribute( 'aria-controls' ) );
+                            content.toggleAttribute( 'hidden' );
+
+                            // Swap the icon
+                            const buttonIcon = trigger.querySelector( '.fas' );
+                            buttonIcon.classList.toggle( 'fa-minus' );
+                            buttonIcon.classList.toggle( 'fa-plus' );
+                        }
+
+                        triggers.forEach( trigger => trigger.addEventListener( 'click', toggleAccordion ) );
+                    } )();
+                </script>
 
                 <?php wp_reset_postdata(); ?>
             </div>
